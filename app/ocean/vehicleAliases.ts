70ec5d3c521d2b69b3dae94fafa3ac6b02323563
Farhat-auto <heurotec@hotmail.com>
2026-09-26 05:52:08 +0100

    Resolve reviewed E90 316i legacy storefront identity

--- app/ocean/vehicleAliases.ts
+++ app/ocean/vehicleAliases.ts
@@ -11,6 +11,8 @@ const REVIEWED_ALIASES: Readonly<Record<string, string>> = {
   "ovh-4d59a21176f5ea9c97da": "ovh-d1bd300d05c7a189edd8",
   "bmw-1-series-coupe-e82-135-i-n54-b30-a-n55-b30-a-225-kw-306-hp-3000-cc-petrol-coupe-10-2007-10-2013":
     "ovh-d1bd300d05c7a189edd8",
+  "bmw-3-series-e90-316-i-n43-b16-a-85-kw-116-hp-cc-petrol-sedan-09-2005-10-2011":
+    "ovh-1fd0868a80585315ddd6",
   "bmw-3-series-e90-316-d-n47-d20-c-85-kw-116-hp-cc-diesel-sedan-07-2009-12-2011":
     "ovh-8a49866f9b684104bfcb",
   "bmw-3-series-e90-316-d-n47-d20-c-85-kw-116-hp-1995-cc-diesel-sedan-07-2009-12-2011":
